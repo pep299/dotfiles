b1487d643766a9ec6ea2bcc070a08da2e12f31a8
pep299 <32212879+pep299@users.noreply.github.com>
2025-03-10 22:22:57 +0900

    add: .gitconfig

--- syml.sh
+++ syml.sh
@@ -1,50 +1,52 @@
 # dotfiles
-ln -s $PWD/zsh/.zprofile $HOME
-ln -s $PWD/zsh/.zshrc $HOME
+ln -sf $PWD/zsh/.zprofile $HOME
+ln -sf $PWD/zsh/.zshrc $HOME
 
 
 # config
 ## karabiner
 mkdir -p $HOME/.config/karabiner
-ln -s $PWD/karabiner/karabiner.json $HOME/.config/karabiner
+ln -sf $PWD/karabiner/karabiner.json $HOME/.config/karabiner
 
 ## git
+ln -sf $PWD/git/.gitconfig $HOME/.gitconfig
 mkdir -p $HOME/.config/git
-ln -s $PWD/git/ignore $HOME/.config/git
+ln -sf $PWD/git/ignore $HOME/.config/git
 
 ## mise
 mkdir -p $HOME/.config/mise
-ln -s $PWD/mise/config.toml $HOME/.config/mise
+ln -sf $PWD/mise/config.toml $HOME/.config/mise
 
 ## colima
 mkdir -p $HOME/.colima/default
-ln -s $PWD/colima/colima.yaml $HOME/.colima/default
+ln -sf $PWD/colima/colima.yaml $HOME/.colima/default
 mkdir -p $HOME/.docker
-ln -s $PWD/colima/config.json $HOME/.docker
+ln -sf $PWD/colima/config.json $HOME/.docker
 
 ## vscode
 mkdir -p $HOME/.vscode/extensions
-ln -s $PWD/vscode/extensions.json $HOME/.vscode/extensions
-ln -s $PWD/vscode/settings.json $HOME/Library/Application\ Support/Code/User
+ln -sf $PWD/vscode/extensions.json $HOME/.vscode/extensions
+ln -sf $PWD/vscode/settings.json $HOME/Library/Application\ Support/Code/User
+
 
 # gnu command
 mkdir -p $HOME/gnubin
-ln -s $(brew --prefix make)/bin/gmake $HOME/gnubin/make
-ln -s $(brew --prefix curl)/bin/curl $HOME/gnubin/curl
-ln -s $(brew --prefix gawk)/bin/gawk $HOME/gnubin/awk
-ln -s $(brew --prefix grep)/bin/ggrep $HOME/gnubin/grep
-ln -s $(brew --prefix gnu-sed)/bin/gsed $HOME/gnubin/sed
-ln -s $(brew --prefix gnu-tar)/bin/gtar $HOME/gnubin/tar
+ln -sf $(brew --prefix make)/bin/gmake $HOME/gnubin/make
+ln -sf $(brew --prefix curl)/bin/curl $HOME/gnubin/curl
+ln -sf $(brew --prefix gawk)/bin/gawk $HOME/gnubin/awk
+ln -sf $(brew --prefix grep)/bin/ggrep $HOME/gnubin/grep
+ln -sf $(brew --prefix gnu-sed)/bin/gsed $HOME/gnubin/sed
+ln -sf $(brew --prefix gnu-tar)/bin/gtar $HOME/gnubin/tar
 
 ## coreutils
-ln -s $(brew --prefix coreutils)/bin/gbase64 $HOME/gnubin/base64
-ln -s $(brew --prefix coreutils)/bin/gcat $HOME/gnubin/cat
-ln -s $(brew --prefix coreutils)/bin/gdate $HOME/gnubin/date
-ln -s $(brew --prefix coreutils)/bin/gecho $HOME/gnubin/echo
-ln -s $(brew --prefix coreutils)/bin/gls $HOME/gnubin/ls
-ln -s $(brew --prefix coreutils)/bin/gmkdir $HOME/gnubin/mkdir
-ln -s $(brew --prefix coreutils)/bin/gpwd $HOME/gnubin/pwd
-ln -s $(brew --prefix coreutils)/bin/gsort $HOME/gnubin/sort
-ln -s $(brew --prefix coreutils)/bin/gtail $HOME/gnubin/tail
-ln -s $(brew --prefix coreutils)/bin/gtouch $HOME/gnubin/touch
-ln -s $(brew --prefix coreutils)/bin/gwhoami $HOME/gnubin/whoami
+ln -sf $(brew --prefix coreutils)/bin/gbase64 $HOME/gnubin/base64
+ln -sf $(brew --prefix coreutils)/bin/gcat $HOME/gnubin/cat
+ln -sf $(brew --prefix coreutils)/bin/gdate $HOME/gnubin/date
+ln -sf $(brew --prefix coreutils)/bin/gecho $HOME/gnubin/echo
+ln -sf $(brew --prefix coreutils)/bin/gls $HOME/gnubin/ls
+ln -sf $(brew --prefix coreutils)/bin/gmkdir $HOME/gnubin/mkdir
+ln -sf $(brew --prefix coreutils)/bin/gpwd $HOME/gnubin/pwd
+ln -sf $(brew --prefix coreutils)/bin/gsort $HOME/gnubin/sort
+ln -sf $(brew --prefix coreutils)/bin/gtail $HOME/gnubin/tail
+ln -sf $(brew --prefix coreutils)/bin/gtouch $HOME/gnubin/touch
+ln -sf $(brew --prefix coreutils)/bin/gwhoami $HOME/gnubin/whoami
